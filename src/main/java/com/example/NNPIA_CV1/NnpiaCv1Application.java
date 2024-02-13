3264593051a6e90c1486e5cea09557338c45320e
Petr Jiruše <petrjiruse@seznam.cz>
2024-02-13 16:32:53 +0100

    initial commit

---
+++ src/main/java/com/example/NNPIA_CV1/NnpiaCv1Application.java
@@ -0,0 +1,13 @@
+package com.example.NNPIA_CV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaCv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpiaCv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NNPIA_CV1/NnpiaCv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NNPIA_CV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
